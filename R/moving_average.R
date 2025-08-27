106cc726aca60b3a32ec399ea5d81addc9f3fb6a
jwonyk <wonyoungkim@ucsb.edu>
2025-08-27 16:45:04 -0700

    before branching

---
+++ R/moving_average.R
@@ -0,0 +1,28 @@
+#' Calculating Moving Average
+#'
+#' @param focal_date 
+#' @param dates 
+#' @param concentration 
+#' @param win_size_wks 
+#'
+#' @returns result
+#' @export
+#'
+#' @examples
+#' ca_ma_once <- moving_average(focal_date, dates, concentration, win_size_wks)
+
+moving_average <- function(focal_date, dates, concentration, win_size_wks) {
+  
+  # Which dates are in the window?
+  is_in_window <- (dates > focal_date - (win_size_wks / 2) * 7) &
+    (dates < focal_date + (win_size_wks / 2) * 7)
+  
+  # Find the associated concentration
+  window_concentration <- concentration[is_in_window]
+  
+  # Calculate the mean
+  result <- mean(window_concentration)
+  
+  return(result)
+  
+}
--- scratch/spaghetti_code.R
+++ scratch/spaghetti_code.R
@@ -16,72 +16,61 @@ library(patchwork)
 library(paletteer)
 library(zoo)
 
-BQ1 <- read_csv(here("data", "QuebradaCuenca1-Bisley.csv"))
-BQ2 <- read_csv(here("data", "QuebradaCuenca2-Bisley.csv"))
-BQ3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv"))
-prm <- read_csv(here("data", "RioMameyesPuenteRoto.csv"))
+moving_average <- function(focal_date, dates, concentration, win_size_wks) {
+  
+  # Which dates are in the window?
+  is_in_window <- (dates > focal_date - (win_size_wks / 2) * 7) &
+    (dates < focal_date + (win_size_wks / 2) * 7)
+  
+  # Find the associated concentration
+  window_concentration <- concentration[is_in_window]
+  
+  # Calculate the mean
+  result <- mean(window_concentration)
+  
+  return(result)
+  
+}
 
-BQ1_years <-  BQ1 %>% 
-  mutate(year = lubridate::year(Sample_Date)) %>% 
-  relocate(year, .after = Code) %>% 
-  group_by(year) %>% 
-  summarise(BQ1_K = mean(`K`, na.rm = TRUE), 
-            BQ1_NO3_N = mean(`NO3-N`, na.rm = TRUE),
-            BQ1_Mg = mean(`Mg`, na.rm = TRUE),
-            BQ1_Ca = mean(`Ca`, na.rm = TRUE),
-            BQ1_NH4_N = mean(`NH4-N`, na.rm = TRUE)) %>% 
+BQ1 <- read_csv(here("data", "QuebradaCuenca1-Bisley.csv")) %>% 
+  clean_names()
+BQ2 <- read_csv(here("data", "QuebradaCuenca2-Bisley.csv")) %>% 
+  clean_names()
+BQ3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv")) %>% 
+  clean_names()
+prm <- read_csv(here("data", "RioMameyesPuenteRoto.csv")) %>% 
+  clean_names()
+
+BQ1_fig_3 <-  BQ1 %>% 
+  mutate(weeks = lubridate::week(sample_date)) %>% 
+  pivot_longer(cols = c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrients", values_to = "concentration") %>% 
+  select(sample_date, sample_id, nutrients, concentration, weeks) %>% 
   ungroup()
 
-BQ2_years <-  BQ2 %>% 
-  mutate(year = lubridate::year(Sample_Date)) %>% 
-  relocate(year, .after = Code) %>% 
-  group_by(year) %>% 
-  summarise(BQ2_K = mean(`K`, na.rm = TRUE), 
-            BQ2_NO3_N = mean(`NO3-N`, na.rm = TRUE),
-            BQ2_Mg = mean(`Mg`, na.rm = TRUE),
-            BQ2_Ca = mean(`Ca`, na.rm = TRUE),
-            BQ2_NH4_N = mean(`NH4-N`, na.rm = TRUE)) %>% 
+BQ2_fig_3 <-  BQ2 %>% 
+  mutate(weeks = lubridate::week(sample_date)) %>% 
+  pivot_longer(cols = c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrients", values_to = "concentration") %>% 
+  select(sample_date, sample_id, nutrients, concentration, weeks) %>% 
   ungroup()
 
-BQ3_years <-  BQ3 %>% 
-  mutate(year = lubridate::year(Sample_Date)) %>% 
-  relocate(year, .after = Code) %>% 
-  group_by(year) %>% 
-  summarise(BQ3_K = mean(`K`, na.rm = TRUE), 
-            BQ3_NO3_N = mean(`NO3-N`, na.rm = TRUE),
-            BQ3_Mg = mean(`Mg`, na.rm = TRUE),
-            BQ3_Ca = mean(`Ca`, na.rm = TRUE),
-            BQ3_NH4_N = mean(`NH4-N`, na.rm = TRUE)) %>% 
+BQ3_fig_3 <-  BQ3 %>% 
+  mutate(weeks = lubridate::week(sample_date)) %>% 
+  pivot_longer(cols = c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrients", values_to = "concentration") %>% 
+  select(sample_date, sample_id, nutrients, concentration, weeks) %>% 
   ungroup()
 
-prm_years <-  prm %>% 
-  mutate(year = lubridate::year(Sample_Date)) %>% 
-  relocate(year, .after = Code) %>% 
-  group_by(year) %>% 
-  summarise(prm_K = mean(`K`, na.rm = TRUE), 
-            prm_NO3_N = mean(`NO3-N`, na.rm = TRUE),
-            prm_Mg = mean(`Mg`, na.rm = TRUE),
-            prm_Ca = mean(`Ca`, na.rm = TRUE),
-            prm_NH4_N = mean(`NH4-N`, na.rm = TRUE)) %>% 
+prm_fig_3 <-  prm %>% 
+  mutate(weeks = lubridate::week(sample_date)) %>% 
+  pivot_longer(cols = c("k", "no3_n", "mg", "ca", "nh4_n"),
+               names_to = "nutrients", values_to = "concentration") %>% 
+  select(sample_date, sample_id, nutrients, concentration, weeks) %>% 
   ungroup()
 
-bqprm <- full_join(BQ1_years, BQ2_years) %>% 
-  full_join(BQ3_years) %>% 
-  full_join(prm_years)
-
-k_join <- bqprm %>% 
-  select(year, BQ1_K, BQ2_K, BQ3_K, prm_K)
-
-no3_N_join <- bqprm %>% 
-  select(year, BQ1_NO3_N, BQ2_NO3_N, BQ3_NO3_N, prm_NO3_N)
-
-Mg_join <- bqprm %>% 
-  select(year, BQ1_Mg, BQ2_Mg, BQ3_Mg, prm_Mg)
-
-Ca_join <- bqprm %>% 
-  select(year, BQ1_Ca, BQ2_Ca, BQ3_Ca, prm_Ca)
-
-NH4_N_join <- bqprm %>% 
-  select(year, BQ1_NH4_N, BQ2_NH4_N, BQ3_NH4_N, prm_NH4_N)
-
-# No changes are made, just testing
+bqprm <- full_join(BQ1_fig_3, BQ2_fig_3) %>%
+  full_join(BQ3_fig_3) %>%
+  full_join(prm_fig_3) %>% 
+  ungroup()
+  # mutate(rolling_average = moving_average(focal_date, dates, concentration, win_size_wks))
